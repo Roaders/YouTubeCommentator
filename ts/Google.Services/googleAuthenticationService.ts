d6fec53dbb4fc40aac93e35304ea473a6ed8e7a6
unknown <giles@roadnight.name>
2019-02-02 18:53:22 +0000

    Got new auth method working

--- ts/Google.Services/googleAuthenticationService.ts
+++ ts/Google.Services/googleAuthenticationService.ts
@@ -1,216 +1,87 @@
+
 /**
  * Created by Giles on 08/02/2016.
  */
 
 module Google.Services {
 
-    import HttpRequest = gapi.client.HttpRequest;
-
-    interface IAppDetails {
-        "web": IClientDetails
-    }
-
-    interface IClientDetails {
-        "client_id": string,
-        "project_id": string,
-        "auth_uri": string,
-        "token_uri": string,
-        "auth_provider_x509_cert_url": string
-    }
-
-    interface IHasResult<T>{
-        result: T
-    }
-
-	// TODO: figure out refresh
     export class GoogleAuthenticationService {
 
         //  Statics
 
-        static cookie_Key: string = "userToken";
+        static $inject = ["$rootScope", "$location", "authorizationScopes"];
 
-		static $inject = ["$http","$rootScope","$location","$cookies","authorizationScopes"];
 
         //  Constructor
 
         constructor(
-            private $http:ng.IHttpService,
-            private $rootScope:ng.IScope,
+            private $rootScope: ng.IScope,
             private $location: ng.ILocationService,
-            private $cookies : angular.cookies.ICookiesService,
-			private authorizationScopes: string
+            private authorizationScopes: string
         ) {
-            if( this.$cookies.get( GoogleAuthenticationService.cookie_Key ) ) {
-                this.token = JSON.parse( this.$cookies.get( GoogleAuthenticationService.cookie_Key ) );
-                console.log( "setting api token from cookie: " + this.token.access_token );
-                gapi.auth.setToken(this.token);
+            console.log(`GoogleAuthenticationService`);
 
-				$location.path( "/comments" );
-            }
+            this.navigateIfAuthenticated();
         }
-
-        //  Private Variables
-
-        private clientDetails: IClientDetails;
-        private clientDetailsStream: Rx.Observable<IClientDetails>;
-        private token: GoogleApiOAuth2TokenObject;
-		private refreshToken: Rx.Observable<any>;
 
         //  Public Functions
 
         logOut(): void {
             console.log("destroying token");
-            gapi.auth.signOut();
+            gapi.auth2.getAuthInstance().disconnect();
 
-            this.$cookies.put( GoogleAuthenticationService.cookie_Key, undefined );
-
-            this.$location.path( "/login" );
+            this.$location.path("/login");
         }
 
-        authenticate():Rx.Observable<GoogleApiOAuth2TokenObject> {
-            console.log( `authenticate scopes: ${this.authorizationScopes}` );
-
-            return this.loadClientDetails()
-                .flatMap<GoogleApiOAuth2TokenObject>(clientDetails => {
-                    return this.askForAuthentication(clientDetails, this.authorizationScopes)
-                })
-                .safeApply(
-                    this.$rootScope,
-                    token => {
-                        console.log(`token received: ${token.access_token}`);
-                        this.$location.path( "/comments" );
-                        this.token = token;
-                    },
-                    error => {
-                        console.log(`Error authorizing access: ${error}`);
-						alert( `There was a problem with authorisation: ${error}` );
-                    });
+        authenticate() {
+            console.log(`authenticate`);
+            return Rx.Observable.fromPromise(gapi.auth2.getAuthInstance().signIn())
+            .safeApply(
+                this.$rootScope,
+                () => {
+                    this.navigateIfAuthenticated();
+                });
         }
 
-		request<T>(args:{ path: string, params?: any, method?: string, body?: any }):Rx.Observable<T> {
-		    //console.log( `make request for: ${args.path}` );
+        request<T>(args: { path: string, params?: any, method?: string, body?: any }) {
+            console.log(`make request for: ${args.path}`);
 
-		    if( !this.token ) {
-		        console.log( "no token, returning to login screen")
-		        this.$location.path( "/login" );
-		        return Rx.Observable.throw<T>( "no token specified" );
-		    }
+            const request = gapi.client.request(args);
 
-		    return this.loadClientDetails()
-		        .flatMap<T>( clientDetails => {
-		            const request:HttpRequest<T> = gapi.client.request(args);
-		            return Rx.Observable.fromPromise<IHasResult<T>>( <any>request)
-		                .map( result => { return result.result } );
-		            }
-		        )
-				.retryWhen( errors => {
+            return Rx.Observable.create<T>(observer => {
+                request.then(success => {
+                    observer.onNext(success.result);
+                    observer.onCompleted();
+                }, error => {
+                    observer.onError(error);
+                    observer.onCompleted();
+                });
+            })
+            .safeApply(
+                this.$rootScope,
+                undefined,
+                error => {
+                    if(error.status === 401){
+                        this.$location.path("/login");
+                    }
+                });
+        }
 
-					return errors.flatMap( currentError => {
+        private navigateIfAuthenticated(){
+            console.log(`navigateIfAuthenticated`);
 
-						console.log( `Error status: ${currentError.result.error.code}` );
+            var user = gapi.auth2.getAuthInstance().currentUser.get();
 
-						if(currentError.result.error.code == 401) {
-					 		console.log( `not authorized, attempting auth for ${args.path}` );
-							if( !this.refreshToken ) {
-								console.log( `creating refresh token for ${args.path}` );
-								this.refreshToken = this.askForAuthentication( this.clientDetails,this.authorizationScopes, true)
-									.do( _ => {
-										console.log( `refresh token complete ${args.path}` );
-										this.refreshToken = null;
-									} ).shareReplay(1);
-							} else {
-								console.log( `returning existing refresh token for ${args.path}` );
-							}
-
-							return this.refreshToken;
-						}
-
-						return Rx.Observable.throw( currentError );
-					} )
-					.do(
-						_ => {console.log(`flatmap onNext for ${args.path}`)},
-						_ => {console.log(`flatmap error for ${args.path}`)},
-						() => {console.log(`flatmap complete for ${args.path}`)}
-				);
-				} )
-		        .do(
-		          _ => {
-					  //console.log(`result returned for path: ${args.path}`)
-				  },
-		          error => { console.log( `error returned for path: ${args.path} Error: ${error}` ) }
-		        );
-		}
-
-        //  Private Functions
-
-        private getPath( args:{ path: string, params?: any } ): string {
-            var urlParams: string = "";
-
-            args.params = args.params || {};
-
-            args.params.access_token = this.token.access_token;
-
-            for( var paramName in args.params ) {
-                urlParams += urlParams === "" ? "?" : "&";
-                urlParams += paramName + "=";
-                urlParams += encodeURIComponent( args.params[paramName] );
+            if (user.hasGrantedScopes(this.authorizationScopes)) {
+                console.log(`scopes are granted`);
+                
+                this.$location.path("/comments");
+            } else {
+                console.log(`not granted scopes`);
+                
+                this.$location.path("/login");
             }
-
-            return args.path + urlParams;
         }
 
-        private askForAuthentication(clientDetails:IClientDetails, scope:string, immediate: boolean = false):Rx.Observable<GoogleApiOAuth2TokenObject> {
-            console.log( `Asking for authentication: ${clientDetails.client_id}` );
-
-            return Rx.Observable.fromCallback(gapi.auth.authorize)(
-                {client_id: clientDetails.client_id, scope: scope, immediate: immediate, authuser: immediate ? 1 : -1}
-            )
-			.map( token => {
-				const newToken: GoogleApiOAuth2TokenObject = {
-					access_token: token.access_token,
-					error: token.error,
-					expires_in: token.expires_in,
-					state: token.state
-				};
-				this.$cookies.put( GoogleAuthenticationService.cookie_Key, JSON.stringify(newToken) );
-				return newToken;
-			} ).do( authorization => {
-				console.log( "authorisation granted" )
-			} );
-        }
-
-        private loadClientDetails():Rx.Observable<IClientDetails> {
-
-            if( this.clientDetails ) {
-                return Rx.Observable.return<IClientDetails>(this.clientDetails);
-            }
-
-            if( !this.clientDetailsStream ) {
-
-                this.clientDetailsStream = Rx.Observable.return<string>( "client_id.json" )
-                    .flatMap<ng.IHttpPromiseCallbackArg<IAppDetails>>( url => {
-                        console.log( "loading client details from url: " + url);
-                        return Rx.Observable.fromPromise<ng.IHttpPromiseCallbackArg<IAppDetails>>( this.$http.get( url ) );
-                    } )
-                    .retry(3)
-					.doOnError( error => {
-						console.log( "Error loading client_id: ")
-						console.dir(error);
-						alert( "There was a problem loading the client id" );
-					} )
-                    .map(callback => {
-                        return callback.data.web;
-                    })
-                    .do(clientDetails => {
-                        console.log( `client details loaded: ${clientDetails.client_id}` );
-                        gapi.client.setApiKey(clientDetails.client_id);
-                        this.clientDetails = clientDetails;
-                    })
-                    .share();
-            }
-
-            return this.clientDetailsStream;
-
-        }
     }
 }
--- ts/app.ts
+++ ts/app.ts
@@ -11,30 +11,53 @@
 /// <reference path="Pricklythistle/controller/commentatorController.ts"/>
 /// <reference path="Pricklythistle/controller/loginController.ts"/>
 /// <reference path="Pricklythistle/controller/headerController.ts"/>
+/// <reference path="../node_modules/@types/gapi/index.d.ts"/>
+/// <reference path="../node_modules/@types/gapi.auth2/index.d.ts"/>
+
+interface IClientId{
+    apiKey: string;
+    client_id: string;
+}
 
 const scopes: string = "https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/youtube.force-ssl";
 
-const app: ng.IModule = angular.module( "youTubeCommentator", [ "ngRoute", "ngCookies", "ngSanitize", "infinite-scroll", "monospaced.elastic" ] );
+const app: ng.IModule = angular.module("youTubeCommentator", ["ngRoute", "ngCookies", "ngSanitize", "infinite-scroll", "monospaced.elastic"]);
 
-app.controller( "commentatorController", Pricklythistle.Controller.CommentatorController )
-	.controller( "loginController", Pricklythistle.Controller.LoginController )
-	.controller( "headerController", Pricklythistle.Controller.HeaderController );
+app.controller("commentatorController", Pricklythistle.Controller.CommentatorController)
+    .controller("loginController", Pricklythistle.Controller.LoginController)
+    .controller("headerController", Pricklythistle.Controller.HeaderController);
 
-app.service( "googleAuthenticationService", Google.Services.GoogleAuthenticationService )
-	.service( "youTubeService", Google.Services.YouTubeService )
-	.service( "commentService", Pricklythistle.Services.CommentService );
+app.service("googleAuthenticationService", Google.Services.GoogleAuthenticationService)
+    .service("youTubeService", Google.Services.YouTubeService)
+    .service("commentService", Pricklythistle.Services.CommentService);
 
-app.config( ['$routeProvider',( $routeProvider: angular.route.IRouteProvider ) => {
+app.config(['$routeProvider', ($routeProvider: angular.route.IRouteProvider) => {
     $routeProvider
-        .when( "/login", { templateUrl:"templates/login.html?v=__applicationVersionNumber__", controller: "loginController as controller" } )
-        .when( "/comments", { templateUrl:"templates/comments.html?v=__applicationVersionNumber__", controller: "commentatorController as commentsController" } )
-        .otherwise( { redirectTo: "/login" } );
-}] );
+        .when("/login", { templateUrl: "templates/login.html?v=__applicationVersionNumber__", controller: "loginController as controller" })
+        .when("/comments", { templateUrl: "templates/comments.html?v=__applicationVersionNumber__", controller: "commentatorController as commentsController" })
+        .otherwise({ redirectTo: "/login" });
+}]);
 
 angular.module('infinite-scroll').value('THROTTLE_MILLISECONDS', 500);
 
-app.value( "authorizationScopes", scopes);
+app.value("authorizationScopes", scopes);
 
 function initiateApp() {
-    angular.bootstrap( document, ["youTubeCommentator"] );
+    gapi.load('client:auth2', loadClientID);
+
+}
+
+function loadClientID(){
+    jQuery.getJSON("./client_id.json").then(initClient);
+}
+
+function initClient(clientId: IClientId) {
+
+    gapi.client.init({
+        apiKey: clientId.apiKey,
+        clientId: clientId.client_id,
+        scope: scopes
+    }).then(function () {
+        angular.bootstrap(document, ["youTubeCommentator"]);
+    });
 }
